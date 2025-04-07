51a43ba278ca383bb98ef665ed64866ec4d8b0ad
abdullah saleem <abdullhasaleem123@gail.com>
2025-04-07 15:36:50 +0500

    update mian.py

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ app.add_url_rule(rule="/app",endpoint="app",view_func=views.app)
 app.add_url_rule(rule="/app/gender/",endpoint="gender",view_func=views.gender,methods=['GET', 'POST'])
 if __name__ == "__main__":
     app.run(debug=True)
+
